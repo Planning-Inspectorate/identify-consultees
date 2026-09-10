726a11d8d467d643db83b97c13a9adf576eb0da0
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2026-09-10 15:10:18 +0100

    feat(infrastructure): add storage account with container

---
+++ infrastructure/storage.tf
@@ -0,0 +1,71 @@
+resource "azurerm_storage_account" "data" {
+  #TODO: Customer Managed Keys
+  #checkov:skip=CKV2_AZURE_1: Customer Managed Keys not implemented yet
+  #checkov:skip=CKV2_AZURE_18: Customer Managed Keys not implemented yet
+  #TODO: Logging
+  #checkov:skip=CKV_AZURE_33: Logging not implemented yet
+  #checkov:skip=CKV2_AZURE_8: Logging not implemented yet
+  #checkov:skip=CKV_AZURE_43: "Ensure Storage Accounts adhere to the naming rules"
+  #checkov:skip=CKV2_AZURE_38: "Ensure soft-delete is enabled on Azure storage account"
+  #checkov:skip=CKV2_AZURE_40: "Ensure storage account is not configured with Shared Key authorization"
+  #checkov:skip=CKV2_AZURE_41: "Ensure storage account is configured with SAS expiration policy"
+  #checkov:skip=CKV_AZURE_206: "Ensure that Storage Accounts use replication" - configured per environment
+  name                             = "pinsstconsultees${local.environment}"
+  resource_group_name              = azurerm_resource_group.primary.name
+  location                         = module.primary_region.location
+  account_tier                     = "Standard"
+  account_replication_type         = var.storage.replication_type
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+  https_traffic_only_enabled       = true
+  min_tls_version                  = "TLS1_2"
+  public_network_access_enabled    = false
+
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_private_endpoint" "storage_account" {
+  name                = "${local.org}-pe-st-${local.resource_suffix}"
+  location            = module.primary_region.location
+  resource_group_name = azurerm_resource_group.primary.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_dns_zone_group {
+    name                 = "${local.org}-pdns-${local.service_name}-${var.environment}"
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.storage.id]
+  }
+
+  private_service_connection {
+    name                           = "${local.org}-psc-${local.resource_suffix}"
+    private_connection_resource_id = azurerm_storage_account.data.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_storage_container" "data" {
+  #TODO: Logging
+  #checkov:skip=CKV2_AZURE_21 Logging not implemented yet
+  name                  = "${local.service_name}-data"
+  storage_account_id    = azurerm_storage_account.data.id
+  container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "storage_function" {
+  scope                = azurerm_storage_container.data.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = module.function_orchestrator.principal_id
+}
+
+resource "azurerm_role_assignment" "storage_app" {
+  scope                = azurerm_storage_container.data.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = module.app_web.principal_id
+}
